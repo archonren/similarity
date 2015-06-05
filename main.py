c1d0f8d05b7600e380428831283ea6899f375971
archonren <archon_ren@hotmail.com>
2015-06-05 10:19:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-import sys
+import argparse
 from similarity import *
-def main(key):
-    x = data()
+def main(key,k,model_path,user_data_path,verbosity):
+    x = data(k,model_path,user_data_path,verbosity)
     x.output(key)
 if __name__ == '__main__':
-    for user in sys.argv[1].split(","):
-        print('for %d'% user)
-        main(user)
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('keys', metavar='N', type=str, nargs='+',
+                   help='the users id')
+    parser.add_argument('--k', type=int,default=1000,
+                   help='number of cluster')
+    parser.add_argument('--model_path', type=str,default='GoogleNews.bin',
+                   help='path to word2vec model')
+    parser.add_argument('--user_data_path', type =str,default='mefi.json',
+                   help='path to user data')
+    parser.add_argument('--verbosity',default=False,
+                   help='show the progress')
+
+    args = parser.parse_args()
+    for user in args.keys:
+        print('for %s'% user)
+        main(user,args.k,args.model_path,args.user_data_path,args.verbosity)
