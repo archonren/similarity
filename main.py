f9db1c79eb01eb2e03fedd07dd5358739c648bb7
archonren <archon_ren@hotmail.com>
2015-06-12 01:12:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import argparse
 from similarity import *
-def main(key,k,model_path,user_data_path,verbosity):
-    x = data(k,model_path,user_data_path,verbosity)
+def main(key,k,model_path,user_data_path,patch_path,verbosity):
+    x = data(k,model_path,user_data_path,patch_path,verbosity)
     x.output(key)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('keys', metavar='N', type=str, nargs='+',
+    parser.add_argument('keys', metavar='key', type=str, nargs='+',
                    help='the users id')
     parser.add_argument('--k', type=int,default=1000,
                    help='number of cluster')
@@ -13,10 +15,12 @@ if __name__ == '__main__':
                    help='path to word2vec model')
     parser.add_argument('--user_data_path', type =str,default='mefi.json',
                    help='path to user data')
+    parser.add_argument('--patch_path', type =str,default=None,
+                   help='path to patch data')
     parser.add_argument('--verbosity',default=False,
                    help='show the progress')
 
     args = parser.parse_args()
     for user in args.keys:
         print('for %s'% user)
-        main(user,args.k,args.model_path,args.user_data_path,args.verbosity)
+        main(user,args.k,args.model_path,args.user_data_path,args.patch_path,args.verbosity)
